4a72fe371572ba73120e9a49962c69d116d5f948
sirtimkeith <tskeith@uvm.edu>
2016-07-14 12:15:07 -0400

    Add files via upload

--- 104.py
+++ 104.py
@@ -1,51 +1,83 @@
 import glob
 import sys
-import csv
 import pandas as pd
 import numpy as np
 import xlrd
 import openpyxl
+import time
 
 
+keptList = []
 bob = glob.glob("*.xlsx")
 def loc_man():
-    user_start = input("\n Hi. Want to process some data? Yeah you do. Please make sure none of the files you want processed are open. Press c to continue, d to skip to processing or e to exit. \n \n")
+    user_start = input("\nHi. Want to process some data? Yeah you do.\nPlease make sure none of the files you want processed are open.\nPress c to continue or e to exit. \n \n")
     if user_start == 'c':
         print ("Scanning the current directory for eligible files (.xlsx)")
-        print (bob)
-        user_contin1 = input("\n Does this list look correct? Because we're going to combine them.  Press c to continue or r to restart")
+        print ("\n".join(bob))
+        user_contin1 = input("\nDoes this list look correct? Because we're going to combine them.\nPress c to continue or r to restart\n")
+        while user_contin1 in bob:
+            bob.remove(user_contin1)
+            print ("\nRemoved ", user_contin1)
+            print ("\nSo now you have ", bob)
+            user_contin1 = input()
         if user_contin1 == 'c':
             concat_man()
-        if user_contin1 == 'd':
-            spreadsheet_man()
         elif user_contin1 == 'r':
             del bob[:]
             loc_man()
     elif user_start == 'e':
-        print ("\n Goodbye.")
+        print ("\nGoodbye.")
 def concat_man():
     df = pd.DataFrame()
     for f in bob:
         data = pd.read_excel(f)
         df = df.append(data)
-    print ("\n Here's your dataframe! \n \n")
-    print (df)
     writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
     df.to_excel(writer, sheet_name='1')
     workbook = writer.book
     worksheet = writer.sheets['1']
-    user_concat = input("Your concatenated file is in the same directory with the name Output.xlsx. You may want to check that all of your data\n has been preserved. Press c to continue or r to restart")
+    user_concat = input("Your concatenated file is in the same directory with the name Output.xlsx.\nYou may want to check that all of your data has been preserved.\nPress c to continue or r to restart\n")
     if user_concat == 'c':
-        return spreadsheet_man()
-        print ("Okay, time to make the data look nice.")
+        print ("Okay, time to make the data look nice.\n")
+        time.sleep(1)
+        spreadsheet_man()
     elif user_concat == 'r':
         return loc_man()
     else:
         print ("Invalid input, sending you to the beginning.")
         loc_man()
-def spreadsheet_man():
-    OUTOFSIGHT = []
-    keptList = []
 
+def spreadsheet_man():
+    time.sleep(3)
+    wb = openpyxl.load_workbook('output.xlsx')
+    sheet = wb.get_sheet_by_name('1')
+    a = sheet['C1']
+    b = sheet['D1']
+    c = sheet['E1']
+    d = sheet['F1']
+    e = sheet['G1']
+    f = sheet['H1']
+    g = sheet['I1']
+    h = sheet['J1']
+    OUTOFSIGHT = [a.value, b.value, c.value, d.value, e.value, f.value, g.value, h.value]
+    print (OUTOFSIGHT)
+    print ("This is the list of columns in output.xlsx.\nType the values you want to keep, or type c to keep them all. \nWhen you're done, press enter to see your list.\n")
+    spreadsheet_user = input()
+    while spreadsheet_user != '':
+        while spreadsheet_user in OUTOFSIGHT:
+            keptList.append(spreadsheet_user)
+            print ("\n \nAdded ", spreadsheet_user, "\n \n")
+            spreadsheet_user = input()
+            if spreadsheet_user == '':
+                print (keptList)
+                spreadsheet_user_contin1 = input ("Here's the list of columns you want to keep.\n Press c to continue and r to restart.\n")
+                if spreadsheet_user_contin1 == 'c':
+                    column_man()
+                if spreadsheet_user_contin1 == 'r':
+                    del keptList[:]
+                    spreadsheet_man()
+                else:
+                    print ("That value wasn't listed, so I'm sending you back.")
+                    spreadsheet_user = input()
 
 loc_man()
--- spreadsheet_man.py
+++ spreadsheet_man.py
@@ -1,6 +1,5 @@
 import openpyxl
 
-
 keptList = []
 def spreadsheet_man():
     wb = openpyxl.load_workbook('output.xlsx')
@@ -15,14 +14,22 @@ def spreadsheet_man():
     h = sheet['J1']
     OUTOFSIGHT = [a.value, b.value, c.value, d.value, e.value, f.value, g.value, h.value]
     print (OUTOFSIGHT)
-
-    spreadsheet_user = input("This is the list of columns in output.xlsx. Type the values you want to keep, or if you want to keep them all press c. When you're done, press enter to see your list.")
-    while spreadsheet_user is not '':
-        if spreadsheet_user in OUTOFSIGHT:
+    print ("This is the list of columns in output.xlsx.\nType the values you want to keep, or type c to keep them all. \nWhen you're done, press enter to see your list.\n")
+    spreadsheet_user = input()
+    while spreadsheet_user != '':
+        while spreadsheet_user in OUTOFSIGHT:
             keptList.append(spreadsheet_user)
-        elif spreadsheet_user == '':
-            print (keptList)
-        else:
-            print ("That value wasn't listed, so I'm sending you back.")
-        spreadsheet_man()
+            print ("\n \nAdded ", spreadsheet_user, "\n \n")
+            spreadsheet_user = input()
+            if spreadsheet_user == '':
+                print (keptList)
+                spreadsheet_user_contin1 = input ("Here's the list of columns you want to keep.\n Press c to continue and r to restart.\n")
+                if spreadsheet_user_contin1 == 'c':
+                    column_man()
+                if spreadsheet_user_contin1 == 'r':
+                    del keptList[:]
+                    spreadsheet_man()
+                else:
+                    print ("That value wasn't listed, so I'm sending you back.")
+                    spreadsheet_user = input()
 spreadsheet_man()
